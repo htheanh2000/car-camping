c472a12fc2228877744f95518c3514f3134d2e66
Theanh0411 <theanh@mogul.sg>
2022-11-22 19:52:07 +0700

    init

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <div style={{height: '100vh', width: '100vw'}}>
+      <Spline scene="https://prod.spline.design/UlpnmhnuRkRRi5mV/scene.splinecode" />
+    </div>
+  );
+}
